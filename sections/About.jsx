9436f65dffaac5698c62eb272a7dd89ffa3922a3
Lundrim <aliulundrim22@gmail.com>
2023-10-18 22:19:16 +0200

    Scroll Button Added

--- sections/About.jsx
+++ sections/About.jsx
@@ -37,10 +37,10 @@ const About = () => (
       <motion.img
         variants={fadeIn("up", "tween", 0.3, 1)}
         src="/arrow-down.svg"
+        alt="arrow down"
         className="w-[18px] h-[28] object-contain mt-[28px]"
       />
     </motion.div>
-    About section
   </section>
 );
 
